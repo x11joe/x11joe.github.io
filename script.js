3e00023c7eec0f9232a7b73fc43ffa0b7ca6423d
x11joe <33452445+x11joe@users.noreply.github.com>
2025-02-05 07:36:45 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1190,368 +1190,33 @@ function toTitleCase(str) {
 }
 
 function setDefaultCommittees() {
-  // A list of known female names from your committees:
-  // Adjust as necessary if we missed someone or if any are incorrectly assumed female.
-  const femaleNames = [
-    "Diane Larson",
-    "Kathy Hogan",
-    "Judy Lee",
-    "Desiree Van Oosting",
-    "Michelle Powers",
-    "Michelle Axtman",
-    "Claire Cory",
-    "Kristin Roers",
-    "Janne Myrdal",
-    "Karla Rose Hanson",
-    "Lisa Meier",
-    "Brandy L. Pyle",
-    "Alisa Mitskog",
-    "Emily O'Brien",
-    "Liz Conmy",
-    "LaurieBeth Hager",
-    "Desiree Morton",
-    "Anna S. Novak",
-    "Cynthia Schreiber-Beck",
-    "Vicky Steiner",
-    "Gretchen Dobervich",
-    "Collette Brown",
-    "Lisa Finley-DeVille",
-    "Karen Grindberg",
-    "Karen M. Rohr",
-    "Mary Schneider",
-    "Christina Wolff",
-    "Donna Henderson",
-    "SuAnn Olson",
-    "Karen Karls",
-    "Carrie McLeod",
-    "Jayme Davis",
-    "Kathy Frelich",
-    "Macy Bolinske",
-    "Karen A. Anderson",
-    "Dori Hauck"
+  /**
+   * If your "defaultCommittees.js" is loaded,
+   * then `window.DEFAULT_COMMITTEES` and `window.FEMALE_NAMES` should exist.
+   * Otherwise, fall back to a short "mini" version below as a demonstration.
+   */
+
+  const fallbackFemaleNames = [
+    "Diane Larson", "Kathy Hogan", "Judy Lee" // etc...
   ];
 
-  // Here are all of your default Senate AND House committees:
-  let rawCommittees = {
-    // ---------- SENATE -----------
-    "Senate Appropriations Committee": [
-      "Brad Bekkedahl - Chairman",
-      "Robert Erbele - Vice Chairman"
+  let fallbackCommittees = {
+    // A short demo version of committees, just so you can see the structure:
+    "Senate Demo Committee": [
+      "Someone - Chairman",
+      "Someone Else - Vice Chairman",
+      "Another Person"
     ],
-    "Senate Education and Environment Committee": [
-      "Ronald Sorvaag - Chairman",
-      "Cole Conley",
-      "Scott Meyer",
-      "Donald Schaible",
-      "Paul J. Thomas"
-    ],
-    "Senate Government Operations Committee": [
-      "Terry M. Wanzek - Chairman",
-      "Randy A. Burckhard",
-      "Michael Dwyer",
-      "Robert Erbele",
-      "Jonathan Sickler"
-    ],
-    "Senate Human Resources Committee": [
-      "Dick Dever - Chairman",
-      "Tim Mathern",
-      "Sean Cleary",
-      "Kyle Davison",
-      "Jeffery J. Magrum"
-    ],
-    "Senate Education Committee": [
-      "Todd Beard - Chairman",
-      "Josh Boschee",
-      "Randy D. Lemm - Vice Chairman",
-      "Michelle Axtman",
-      "Justin Gerhardt",
-      "Mike Wobbema"
-    ],
-    "Senate Finance and Taxation Committee": [
-      "Mark F. Weber - Chairman",
-      "Richard Marcellais",
-      "Dean Rummel - Vice Chairman",
-      "Dale Patten",
-      "Michelle Powers",
-      "Chuck Walen"
-    ],
-    "Senate Human Services Committee": [
-      "Judy Lee - Chairman",
-      "Kathy Hogan",
-      "Kent Weston - Vice Chairman",
-      "David A. Clemens",
-      "Kristin Roers",
-      "Desiree Van Oosting"
-    ],
-    "Senate Industry and Business Committee": [
-      "Jeff Barta - Chairman",
-      "Keith Boehm - Vice Chairman",
-      "Mark Enget",
-      "Greg Kessel",
-      "Jerry Klein"
-    ],
-    "Senate Judiciary Committee": [
-      "Diane Larson - Chairman",
-      "Ryan Braunberger",
-      "Bob Paulson - Vice Chairman",
-      "Jose L. Castaneda",
-      "Claire Cory",
-      "Larry Luick",
-      "Janne Myrdal"
-    ],
-    "Senate Agriculture and Veterans Affairs Committee": [
-      "Larry Luick - Chairman",
-      "Richard Marcellais",
-      "Janne Myrdal - Vice Chairman",
-      "Randy D. Lemm",
-      "Mark F. Weber",
-      "Kent Weston"
-    ],
-    "Senate Energy and Natural Resources Committee": [
-      "Dale Patten - Chairman",
-      "Greg Kessel - Vice Chairman",
-      "Todd Beard",
-      "Keith Boehm",
-      "Mark Enget",
-      "Justin Gerhardt",
-      "Desiree Van Oosting"
-    ],
-    "Senate State and Local Government Committee": [
-      "Kristin Roers - Chairman",
-      "Ryan Braunberger",
-      "Jose L. Castaneda - Vice Chairman",
-      "Jeff Barta",
-      "Judy Lee",
-      "Chuck Walen"
-    ],
-    "Senate Transportation Committee": [
-      "David A. Clemens - Chairman",
-      "Kathy Hogan",
-      "Claire Cory - Vice Chairman",
-      "Jerry Klein",
-      "Bob Paulson",
-      "Dean Rummel"
-    ],
-    "Senate Workforce Development Committee": [
-      "Mike Wobbema - Chairman",
-      "Josh Boschee",
-      "Michelle Axtman - Vice Chairman",
-      "Diane Larson",
-      "Michelle Powers"
-    ],
-
-    // ---------- HOUSE -----------
-    // House Appropriations
-    "House Appropriations Committee": [
-      "Don Vigesaa - Chairman",
-      "Keith Kempenich - Vice Chairman"
-    ],
-    "House Appropriations Education & Environment Division": [
-      "Mike Nathe - Chairman",
-      "Karla Rose Hanson",
-      "Steve Swiontek - Vice Chairman",
-      "Scott Louser",
-      "Bob Martinson",
-      "David Richter",
-      "Mark Sanford"
-    ],
-    "House Appropriations Government Operations Division": [
-      "David Monson - Chairman",
-      "Mike Brandenburg - Vice Chairman",
-      "Glenn Bosch",
-      "Jay Fisher",
-      "Keith Kempenich",
-      "Lisa Meier",
-      "Brandy L. Pyle"
-    ],
-    "House Appropriations Human Resources Division": [
-      "Jon O. Nelson - Chairman",
-      "Alisa Mitskog",
-      "Gregory Stemen - Vice Chairman",
-      "Bert Anderson",
-      "Mike Berg",
-      "Eric J. Murphy",
-      "Emily O'Brien",
-      "Scott Wagner"
-    ],
-
-    // House Education
-    "House Education Committee": [
-      "Pat D. Heinert - Chairman",
-      "Liz Conmy",
-      "Cynthia Schreiber-Beck - Vice Chairman",
-      "LaurieBeth Hager",
-      "Patrick R. Hatlestad",
-      "Dori Hauck",
-      "Matthew Heilman",
-      "Jim Jonas",
-      "Donald W. Longmuir",
-      "Roger A. Maki",
-      "Andrew Marschall",
-      "Desiree Morton",
-      "Anna S. Novak",
-      "Doug Osowski"
-    ],
-
-    // House Finance and Taxation
-    "House Finance and Taxation Committee": [
-      "Craig Headland - Chairman",
-      "Austin Foss",
-      "Jared C. Hagert - Vice Chairman",
-      "Zachary Ista",
-      "Dick Anderson",
-      "Jason Dockter",
-      "Ty Dressler",
-      "Jim Grueneich",
-      "Mike Motschenbacher",
-      "Dennis Nehring",
-      "Jeremy L. Olson",
-      "Todd Porter",
-      "Vicky Steiner",
-      "Nathan Toman"
-    ],
-
-    // House Human Services
-    "House Human Services Committee": [
-      "Matthew Ruby - Chairman",
-      "Jayme Davis",
-      "Kathy Frelich - Vice Chairman",
-      "Gretchen Dobervich",
-      "Karen A. Anderson",
-      "Mike Beltz",
-      "Macy Bolinske",
-      "Clayton Fegley",
-      "Jared Hendrix",
-      "Dawson Holle",
-      "Dwight Kiefert",
-      "Nico Rios",
-      "Karen M. Rohr"
-    ],
-
-    // House Industry, Business, and Labor
-    "House Industry, Business and Labor Committee": [
-      "Jonathan Warrey - Chairman",
-      "Collette Brown",
-      "Mitch Ostlie - Vice Chairman",
-      "Lisa Finley-DeVille",
-      "Landon Bahl",
-      "Josh Christy",
-      "Karen Grindberg",
-      "Jorin Johnson",
-      "Jim Kasper",
-      "Ben Koppelman",
-      "Dan Ruby",
-      "Mike Schatz",
-      "Austen Schauer",
-      "Daniel R. Vollmer"
-    ],
-
-    // House Judiciary
-    "House Judiciary Committee": [
-      "Lawrence R. Klemin - Chairman",
-      "Mary Schneider",
-      "Karen Karls - Vice Chairman",
-      "Steve Vetter - Vice Chairman",
-      "Nels Christianson",
-      "Donna Henderson",
-      "Jeff Hoverson",
-      "Daniel Johnston",
-      "Carrie McLeod",
-      "SuAnn Olson",
-      "Bernie Satrom",
-      "Bill Tveit",
-      "Lori VanWinkle",
-      "Christina Wolff"
-    ],
-
-    // House Agriculture
-    "House Agriculture Committee": [
-      "Mike Beltz - Chairman",
-      "Gretchen Dobervich",
-      "Dori Hauck - Vice Chairman",
-      "Karen A. Anderson",
-      "Donna Henderson",
-      "Dawson Holle",
-      "Jeff Hoverson",
-      "Dwight Kiefert",
-      "Dennis Nehring",
-      "SuAnn Olson",
-      "Nico Rios",
-      "Cynthia Schreiber-Beck",
-      "Bill Tveit",
-      "Daniel R. Vollmer"
-    ],
-
-    // House Energy and Natural Resources
-    "House Energy and Natural Resources Committee": [
-      "Todd Porter - Chairman",
-      "Liz Conmy",
-      "Dick Anderson - Vice Chairman",
-      "Austin Foss",
-      "Anna S. Novak - Vice Chairman",
-      "Jason Dockter",
-      "Jared C. Hagert",
-      "Craig Headland",
-      "Pat D. Heinert",
-      "Jorin Johnson",
-      "Andrew Marschall",
-      "Jeremy L. Olson",
-      "Matthew Ruby"
-    ],
-
-    // House Government and Veterans Affairs
-    "House Government and Veterans Affairs Committee": [
-      "Austen Schauer - Chairman",
-      "Collette Brown",
-      "Bernie Satrom - Vice Chairman",
-      "Mary Schneider",
-      "Landon Bahl",
-      "Josh Christy",
-      "Karen Grindberg",
-      "Karen Karls",
-      "Carrie McLeod",
-      "Karen M. Rohr",
-      "Vicky Steiner",
-      "Lori VanWinkle",
-      "Steve Vetter",
-      "Christina Wolff"
-    ],
-
-    // House Political Subdivisions
-    "House Political Subdivisions Committee": [
-      "Donald W. Longmuir - Chairman",
-      "Jayme Davis",
-      "Clayton Fegley - Vice Chairman",
-      "LaurieBeth Hager",
-      "Jim Jonas - Vice Chairman",
-      "Macy Bolinske",
-      "Patrick R. Hatlestad",
-      "Matthew Heilman",
-      "Lawrence R. Klemin",
-      "Mike Motschenbacher",
-      "Mitch Ostlie",
-      "Nathan Toman",
-      "Jonathan Warrey"
-    ],
-
-    // House Transportation
-    "House Transportation Committee": [
-      "Dan Ruby - Chairman",
-      "Lisa Finley-DeVille",
-      "Jim Grueneich - Vice Chairman",
-      "Nels Christianson",
-      "Ty Dressler",
-      "Kathy Frelich",
-      "Jared Hendrix",
-      "Daniel Johnston",
-      "Jim Kasper",
-      "Ben Koppelman",
-      "Roger A. Maki",
-      "Desiree Morton",
-      "Doug Osowski",
-      "Mike Schatz"
+    "House Demo Committee": [
+      "Somebody - Chairman",
+      "Different Person - Vice Chairman"
     ]
   };
+
+  // Use the large data from defaultCommittees.js if available,
+  // otherwise fall back to the short "demo" version:
+  let rawCommittees = window.DEFAULT_COMMITTEES || fallbackCommittees;
+  let femaleNames = window.FEMALE_NAMES || fallbackFemaleNames;
 
   // We'll build a new object "transformedCommittees"
   let transformedCommittees = {};
@@ -1575,7 +1240,6 @@ function setDefaultCommittees() {
 
   alert("Default committees have been set!");
 }
-
 
 function transformMemberLine(line, committeeName, femaleNames) {
   // Trim any leading/trailing spaces
@@ -1614,6 +1278,7 @@ function transformMemberLine(line, committeeName, femaleNames) {
     }
   }
 }
+
 
 
 // Support Ctrl + Enter to copy
